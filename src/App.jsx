71ba8fbffe531f9caa00d661ac395b9f00a83e80
Евгений <wfh257@gmail.com>
2026-08-31 17:52:20 +0300

    challenge 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
@@ -14,6 +14,16 @@ const Content = (props) => {
       <p>
         {props.part3} {props.exercises3}
       </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part1={props.part1} exercises1={props.exercises1} />
+      <Part part2={props.part2} exercises2={props.exercises2} />
+      <Part part3={props.part3} exercises3={props.exercises3} />
     </>
   );
 };
