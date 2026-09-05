70a2ca4b274f02bb52d6d583e76f8a1b250540f3
Евгений <wfh257@gmail.com>
2026-09-05 10:54:39 +0300

    challenge 2.2

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -14,12 +14,21 @@ const Content = ({ course }) => {
   return course.parts.map((part) => <Part key={part.id} part={part} />);
 };
 
+const Total = ({ course }) => {
+  const total =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises +
+    course.parts[3].exercises;
+  return <h4>total of {total} exercises</h4>;
+};
+
 const Course = ({ course }) => {
   return (
     <>
       <Header course={course} />
       <Content course={course} />
-      {/* <Total course={course} /> */}
+      <Total course={course} />
     </>
   );
 };
