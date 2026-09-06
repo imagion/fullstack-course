26c902895dcca1f66eae6d53f00c94bbeb72d9ea
Евгений <wfh257@gmail.com>
2026-09-06 08:36:26 +0300

    challenge 2.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
 
     const findDup = persons.find((person) => person.name === newName);
@@ -17,6 +21,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson));
       setNewName('');
+      setNewNumber('');
     }
   };
 
@@ -29,13 +34,22 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{' '}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <div key={person.name}>{person.name}</div>
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
