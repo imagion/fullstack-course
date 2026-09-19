d4f36aaa58969db1bbade7bf47dbb41d2f76123a
Евгений <wfh257@gmail.com>
2026-09-19 08:59:56 +0300

    challenge 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+if (process.argv.length < 4) {
+  console.log('give name as argument');
+  process.exit(1);
+}
+
+if (process.argv.length < 5) {
+  console.log('give phone as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const phone = process.argv[4];
+
+const url = `mongodb+srv://admin:${password}@cluster0.uvqjlby.mongodb.net/?appName=Cluster0`;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+  name: name,
+  number: phone,
+});
+
+person.save().then((result) => {
+  console.log('person saved!');
+  mongoose.connection.close();
+});
