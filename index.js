b41afdd10dcf1ea60f3182db95038f8e5f664508
Евгений <wfh257@gmail.com>
2026-09-20 14:57:35 +0300

    challenge 3.13-3.14

--- index.js
+++ index.js
@@ -1,50 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
+const Person = require('./models/person');
 
 const app = express();
-app.use(express.static('dist'));
 
+app.use(express.static('dist'));
 app.use(express.json());
 
-morgan.token('data', (req) => {
-  console.log(typeof req.body, req.body);
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
-
-app.use(
-  morgan(`:method :url :status :res[content-length] - :response-time ms :data`),
-);
-morgan.token('data', (req) => {
-  console.log(typeof req.body, req.body);
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
-
-app.use(
-  morgan(`:method :url :status :res[content-length] - :response-time ms :data`),
-);
-
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+let persons = [];
 
 const currentTime = new Date();
 
@@ -58,14 +21,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => res.json(persons));
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  person ? res.json(person) : res.status(404).end();
+  Person.findById(req.params.id).then((person) => {
+    res.json(person);
+  });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -91,15 +53,15 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 app.get('/api/info', (req, res) => {
@@ -109,7 +71,7 @@ app.get('/api/info', (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -1,51 +1,51 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// if (process.argv.length === 2) {
-//   console.log('give password as argument');
-//   process.exit(1);
-// }
+// // if (process.argv.length === 2) {
+// //   console.log('give password as argument');
+// //   process.exit(1);
+// // }
 
-// if (process.argv.length > 2 && process.argv.length < 4) {
-//   console.log('give name as argument');
-//   process.exit(1);
-// }
+// // if (process.argv.length > 2 && process.argv.length < 4) {
+// //   console.log('give name as argument');
+// //   process.exit(1);
+// // }
 
-// if (process.argv.length < 5) {
-//   console.log('give phone as argument');
-//   process.exit(1);
-// }
+// // if (process.argv.length < 5) {
+// //   console.log('give phone as argument');
+// //   process.exit(1);
+// // }
 
-const password = process.argv[2];
-const name = process.argv[3];
-const phone = process.argv[4];
+// const password = process.argv[2];
+// const name = process.argv[3];
+// const phone = process.argv[4];
 
-const url = `mongodb+srv://admin:${password}@cluster0.uvqjlby.mongodb.net/?appName=Cluster0`;
+// const url = `mongodb+srv://admin:${password}@cluster0.uvqjlby.mongodb.net/?appName=Cluster0`;
 
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
-mongoose.connect(url, { family: 4 });
+// mongoose.connect(url, { family: 4 });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-const Person = mongoose.model('Person', personSchema);
-
-// const person = new Person({
-//   name: name,
-//   number: phone,
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
 // });
 
-// person.save().then((result) => {
-//   console.log('person saved!');
+// const Person = mongoose.model('Person', personSchema);
+
+// // const person = new Person({
+// //   name: name,
+// //   number: phone,
+// // });
+
+// // person.save().then((result) => {
+// //   console.log('person saved!');
+// //   mongoose.connection.close();
+// // });
+
+// Person.find({}).then((result) => {
+//   console.log(`phonebook:`);
+//   result.forEach((person) => {
+//     console.log(`${person.name} ${person.number}`);
+//   });
 //   mongoose.connection.close();
 // });
-
-Person.find({}).then((result) => {
-  console.log(`phonebook:`);
-  result.forEach((person) => {
-    console.log(`${person.name} ${person.number}`);
-  });
-  mongoose.connection.close();
-});
