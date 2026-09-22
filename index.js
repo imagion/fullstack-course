e4866296a70345c1c758930fd84329fee49f8bee
JN <wfh257@gmail.com>
2026-09-22 16:58:51 +0300

    challenge 4.1

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
-const blogSchema = mongoose.Schema({
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+const mongoUrl = process.env.MONGODB_URI;
+
+console.log('connecting to', mongoUrl);
+mongoose
+  .connect(mongoUrl, { family: 4 })
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
 });
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl, { family: 4 });
-
-app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -24,14 +44,21 @@ app.get('/api/blogs', (request, response) => {
 });
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
+  const body = new Blog(request.body);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
 
   blog.save().then((result) => {
     response.status(201).json(result);
   });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
