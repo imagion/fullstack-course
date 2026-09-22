6578417b93fbdfe272ebe1c129d3c81de713b1f4
Евгений <wfh257@gmail.com>
2026-09-22 08:33:51 +0300

    challenge 4 init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig([
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-  { ignores: ['dist/**'] },
 ]);
--- index.js
+++ index.js
@@ -1,116 +1,37 @@
-require('dotenv').config();
 const express = require('express');
-const Person = require('./models/person');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(express.static('dist'));
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = 'mongodb://localhost/bloglist';
+mongoose.connect(mongoUrl, { family: 4 });
+
 app.use(express.json());
 
-let persons = [];
-
-const currentTime = new Date();
-
-const generateId = () => {
-  const maxId = Math.floor(Math.random() * 999999999);
-  return String(maxId + 1);
-};
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
-
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then((persons) => res.json(persons));
-});
-
-app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then((person) => {
-      if (person) {
-        res.json(person);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/persons', (req, res, next) => {
-  const body = req.body;
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: 'The name or number is missing',
-    });
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  });
-
-  person
-    .save()
-    .then((savedPerson) => {
-      res.json(savedPerson);
-    })
-    .catch((error) => next(error));
-});
-
-app.put('/api/persons/:id', (req, res, next) => {
-  const { name, number } = req.body;
-
-  Person.findById(req.params.id)
-    .then((person) => {
-      if (!person) {
-        return res.status(404).end();
-      }
-
-      person.name = name;
-      person.number = number;
-
-      return person.save().then((updatedPerson) => {
-        res.json(updatedPerson);
-      });
-    })
-    .catch((error) => next(error));
-});
-
-app.get('/api/info', (req, res) => {
-  Person.find({}).then((persons) => {
-    res.send(`
-      <div>Phonebook has info for ${persons.length} people</div>
-      <div>${currentTime}</div>
-    `);
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
   });
 });
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
-  }
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-  next(error);
-};
-
-// this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
+const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
