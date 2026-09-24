a62727b5fe25377032497260153a24f302a9d06d
JN <wfh257@gmail.com>
2026-09-24 07:05:49 +0300

    challenge 4.4

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,7 +1,6 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert');
 const listHelper = require('../utils/list_helper');
-const express = require('express');
 
 describe('dummy test', () => {
   test('dummy returns one', () => {
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,11 +1,18 @@
 const dummy = (blogs) => {
-  if (Array.isArray(blogs)) {
-    return 1;
-  } else {
-    return typeof blogs;
-  }
+  Array.isArray(blogs) ? 1 : typeof blogs;
+};
+
+const totalLikes = (blogs) => {
+  let likes = [];
+
+  blogs.forEach((blog) => {
+    likes.push(blog.likes);
+  });
+
+  return likes.reduce((acc, red) => acc + red, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
